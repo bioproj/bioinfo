8a10009743e572921107acb611da23c12ea9bc20
wangyang <1749748955@qq.com>
2024-05-04 18:44:02 +0800

    update

--- transcriptomics/downlaodTestData.sh
+++ transcriptomics/downlaodTestData.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-wget -O testData https://bioinfo.online/pay/transcriptomics.test.data.55fe0636-c9f9-4300-852e-14b3b9c86f3b.zip
+wget -O testData.zip https://bioinfo.online/pay/transcriptomics.test.data.55fe0636-c9f9-4300-852e-14b3b9c86f3b.zip
+unzip testData.zip
+rm testData.zip
